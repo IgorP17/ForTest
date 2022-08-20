e11d0658ec616d64588c45e768ed9d733159cd91
Игорь Прохоров <iprokhorovster@gmail.com>
2022-08-20 13:27:44 +0300

    wiremock port to 8081

--- src/main/java/wiremock/Settings.java
+++ src/main/java/wiremock/Settings.java
@@ -2,7 +2,7 @@ package wiremock;
 
 public class Settings {
 
-    public final static String baseURI = "http://igor-virtualbox:8080";
+    public final static String baseURI = "http://igor-virtualbox:8081";
     public final static String getPath = "/test";
     public final static String postPath = "/post";
     public final static String getPathParams = "?myid=123";
